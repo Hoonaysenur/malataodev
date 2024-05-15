f0cfe68841e9c3f46af29a43f3505d0f9b21e7ba
Hoonaysenur <aysenurkorkut8@gmail.com>
2024-05-15 18:19:47 +0300

    değişiklikler yapıldı

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Formun varsayılan davranışını engelle
+  event.preventDefault();
   
-  // Kullanıcı adı ve şifre al
+  
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
 
-  // Kullanıcı adı ve şifrenin doğru olup olmadığını kontrol et
-  if (username === 'ayse' && password === '123456') {
-    // Doğruysa giriş başarılı mesajını göster
-    document.getElementById('message').innerText = 'Hoşgeldiniz ' + username;
-    // 2 saniye sonra anasayfaya yönlendir
+  
+  if (username === 'aysenur.korkut1@ogr.sakarya.edu.tr' && password === 'b224400373') {
+    
+    document.getElementById('message').innerText = 'Hoşgeldin' + password;
+    
     setTimeout(function() {
       window.location.href = "anasayfa.html";
     }, 2000);
   } else {
-    // Değilse hata mesajı göster
+    
     document.getElementById('message').innerText = 'Kullanıcı adı veya şifre yanlış!';
   }
 });
